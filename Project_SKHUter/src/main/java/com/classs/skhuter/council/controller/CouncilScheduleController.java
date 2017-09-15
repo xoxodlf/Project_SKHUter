0c0d3440fc25f3b35ff61ac91986bc0b45416369
NaSangYeon <NaSangYeon@192.168.10.3>
2017-09-15 23:38:25 +0900

    학생회 일정 타임라인 추가

--- Project_SKHUter/src/main/java/com/classs/skhuter/council/controller/CouncilScheduleController.java
+++ Project_SKHUter/src/main/java/com/classs/skhuter/council/controller/CouncilScheduleController.java
@@ -1,5 +1,32 @@
 package com.classs.skhuter.council.controller;
 
-public class CouncilScheduleController {
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.classs.skhuter.notice.controller.VoteController;
+
+@Controller
+public class CouncilScheduleController {
+	private static final Logger logger = LoggerFactory.getLogger(VoteController.class);
+	
+	@RequestMapping("/council/*")
+	public String TimeLine(Locale locale, Model model) {
+		logger.info("여기는 학생회 일정!!! The client locale is {}.", locale);
+		
+		Date date = new Date();
+		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
+		
+		String formattedDate = dateFormat.format(date);
+		
+		model.addAttribute("serverTime", formattedDate );
+		
+		return "council/councilSchedule.lay";
+	}
 }
