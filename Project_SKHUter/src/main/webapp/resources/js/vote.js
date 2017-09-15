1c1a93e5a98e2aa42e55d3f3baf83f3584cfd087
xoxod <xoxod@192.168.10.6>
2017-09-16 03:34:19 +0900

    vote js 작성, vote jsp수정

---
+++ Project_SKHUter/src/main/webapp/resources/js/vote.js
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+
+function openCity(vote,elmnt,color) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablink");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].style.backgroundColor = "";
+    }
+    document.getElementById(vote).style.display = "block";
+    elmnt.style.backgroundColor = color;
+
+}
