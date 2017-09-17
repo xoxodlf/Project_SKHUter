5374c885595269dd8f4ebcbc28e2134e75664a0c
xoxod <xoxod@DESKTOP-4TQM41E>
2017-09-17 21:52:45 +0900

    유저컨트롤러

--- Project_SKHUter/src/main/java/com/classs/skhuter/user/controller/UserController.java
+++ Project_SKHUter/src/main/java/com/classs/skhuter/user/controller/UserController.java
@@ -1,5 +1,15 @@
 package com.classs.skhuter.user.controller;
 
-public class UserController {
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
+@Controller
+public class UserController {
+	
+	@RequestMapping(value="/notice/userList", method=RequestMethod.GET)
+	public String noticeList(Model model) {
+		return "notice/userList.lay";
+	}
 }
