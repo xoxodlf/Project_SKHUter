34a7bf3214aa4cfbaaae190dbe83db50512f24b4
user <user@DESKTOP-96GJJ0O>
2017-09-23 04:41:58 +0900

    보드기능구현테스트에러

--- Project_SKHUter/src/main/java/com/classs/skhuter/board/dao/BoardDAOImpl.java
+++ Project_SKHUter/src/main/java/com/classs/skhuter/board/dao/BoardDAOImpl.java
@@ -1,5 +1,50 @@
 package com.classs.skhuter.board.dao;
 
+import java.util.List;
+
+import javax.inject.Inject;
+
+import org.apache.ibatis.session.SqlSession;
+import org.apache.log4j.Logger;
+
+import com.classs.skhuter.board.domain.BoardDTO;
+
 public class BoardDAOImpl implements BoardDAO {
+	@Inject
+	private SqlSession sqlSession;
+
+	private static String namespace = "com.classs.skhuter.mappers.BoardMapper";
+
+	Logger logger = Logger.getLogger(BoardDAOImpl.class);
+
+	@Override
+	public void create(BoardDTO board) {
+		// TODO Auto-generated method stub
+		sqlSession.insert(namespace + ".create", board);
+	}
+
+	@Override
+	public BoardDTO read(int boardNo) throws Exception {
+		// TODO Auto-generated method stub
+		return sqlSession.selectOne(namespace + ".get", boardNo);
+	}
+
+	@Override
+	public void delete(int boardNo) throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public List<BoardDTO> listAll() throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<BoardDTO> listPage(int page) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
--- Project_SKHUter/src/main/java/com/classs/skhuter/board/service/BoardServiceImpl.java
+++ Project_SKHUter/src/main/java/com/classs/skhuter/board/service/BoardServiceImpl.java
@@ -1,5 +1,17 @@
 package com.classs.skhuter.board.service;
 
+import javax.inject.Inject;
+
+import com.classs.skhuter.board.dao.BoardDAO;
+import com.classs.skhuter.board.domain.BoardDTO;
+
 public class BoardServiceImpl implements BoardService {
+	@Inject
+	private BoardDAO dao;
+
+	@Override
+	public void create(BoardDTO board) {
+		dao.create(board);
+	}
 
 }
