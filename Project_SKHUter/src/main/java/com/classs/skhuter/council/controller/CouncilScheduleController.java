da64455078c82a534d50037e11a8029a820b6062
JONG-YOON <jongyoon9515@gmail.com>
2017-10-09 14:45:01 +0900

    일정 부분 모바일용 컨트롤러 등록 / 주석 처리

--- Project_SKHUter/src/main/java/com/classs/skhuter/council/controller/CouncilScheduleController.java
+++ Project_SKHUter/src/main/java/com/classs/skhuter/council/controller/CouncilScheduleController.java
@@ -1,13 +1,11 @@
 package com.classs.skhuter.council.controller;
 
-import java.text.DateFormat;
 import java.util.Calendar;
-import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
-import java.util.Locale;
+import java.util.Map;
 
 import javax.inject.Inject;
-import javax.servlet.http.HttpServletRequest;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -16,11 +14,22 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.classs.skhuter.council.domain.CouncilScheduleDTO;
 import com.classs.skhuter.council.service.CouncilScheduleService;
+import com.classs.skhuter.user.domain.UserDTO;
 
+/**
+ * 학생회 일정 관련 프로젝트 Controller @RequestMapping("/council/councilSchedule") URI 매칭
+ * 
+ * @패키지 : com.classs.skhuter.council.controller
+ * @파일명 : CouncilScheduleController.java
+ * @작성자 : 나상연
+ * @작성일 : 2017. 10. 3. 
+ *
+ */
 @Controller
 @RequestMapping("/council")
 public class CouncilScheduleController {
@@ -41,6 +50,13 @@ public class CouncilScheduleController {
 	public String currentM = Integer.toString(cal.get(Calendar.MONTH)+1);
 	
 	
+	/**
+	 * 학생회 일정 전체 목록 조회
+	 *
+	 * @Method Name : TimeLine
+	 * @param model
+	 * @return
+	 */
 	@RequestMapping("/councilSchedule")
 	public String TimeLine(Model model) {
 		logger.info("여기는 학생회 일정!!!");
@@ -52,48 +68,73 @@ public class CouncilScheduleController {
 		System.out.println("선택달 첫번째 날: "+thisMonth);
 		System.out.println("선택달의 다음달 첫번째 날: "+nextMonth);
 		
-		model.addAttribute("scheduleList",scheduleList);
-		model.addAttribute("currentDate",currentDate);
-		model.addAttribute("currentY",currentY);
-		model.addAttribute("currentM",currentM);
-		model.addAttribute("thisMonth",thisMonth);
-		model.addAttribute("nextMonth",nextMonth);
+		model.addAttribute("scheduleList", scheduleList);
+		model.addAttribute("currentDate", currentDate);
+		model.addAttribute("currentY", currentY);
+		model.addAttribute("currentM", currentM);
+		model.addAttribute("thisMonth", thisMonth);
+		model.addAttribute("nextMonth", nextMonth);
 		
 		return "council/councilSchedule.lay";
 	}
 	
+	/**
+	 * 학생회 일정 등록
+	 *
+	 * @Method Name : registSchedule
+	 * @param registeredSchedule
+	 * @param rttr
+	 * @return
+	 */
 	@RequestMapping(value="/councilSchedule/registSchedule", method = RequestMethod.POST)
 	public String registSchedule(CouncilScheduleDTO registeredSchedule,RedirectAttributes rttr) {
-		logger.info(registeredSchedule.toString());
+
 		service.register(registeredSchedule);
 		
 		rttr.addFlashAttribute("message", "createsuccess");
+		
 		return "redirect:/council/councilSchedule";	
 	}
 	
+	/**
+	 * 학생회 일정 삭제
+	 *
+	 * @Method Name : deleteSchedule
+	 * @param councilScheduleNo
+	 * @param rttr
+	 * @return
+	 */
 	@RequestMapping(value="/councilSchedule/deleteSchedule", method = RequestMethod.POST)
 	public String deleteSchedule(@RequestParam("councilScheduleNo") int councilScheduleNo,RedirectAttributes rttr) {
-		logger.info(councilScheduleNo+"");
+		
 		service.deleteSchedule(councilScheduleNo);
+		
 		rttr.addFlashAttribute("message", "deletesuccess");
+		
 		return "redirect:/council/councilSchedule";
 	}
 	
+	/**
+	 * 날짜 변경
+	 *
+	 * @Method Name : changeDate
+	 * @param changeYear	변경한 해
+	 * @param changeMonth	변경한 달
+	 * @param rttr
+	 * @param model
+	 * @return
+	 */
 	@RequestMapping(value="/councilSchedule/changeDate",method = RequestMethod.POST)
 	public String changeDate(String changeYear,String changeMonth,RedirectAttributes rttr,Model model) {
 		currentY = changeYear;
 		currentM = changeMonth;
 		
-		/**
-		 * 일정이 끝나는 달을 고정시키기 위한 연산.
-		 * 
-		 * 
-		 */
+		/** 일정이 끝나는 달을 고정시키기 위한 연산 */
 		thisMonth = changeYear+"-"+(Integer.parseInt(changeMonth)<10?"0"+Integer.parseInt(changeMonth):
 			Integer.parseInt(changeMonth))+"-"+"01"+" "+"00"+":"+"00"+":"+"00";
 		
 		/** 
-		 * 일정이 끝나는 달까지만 표시해 주기 위한 연산.
+		 * 일정이 끝나는 달까지만 표시해 주기 위한 연산
 		 * 
 		 * 첫줄 Integer.parseInt(changeMonth)+1<10 는 사용자가 선택한 달이 10보다 낮으면
 		 * changeYear+"-"+"0"+(Integer.parseInt(changeMonth)+1) 에서 "0" 부분을 추가하여 01~09 형식으로 바꿔준다.
@@ -101,17 +142,40 @@ public class CouncilScheduleController {
 		 * Integer.parseInt(changeYear)+1)+"-"+"01" 로 년수를 1 증가시키고 달을 1월달로 만들어준다.
 		 * 세번째줄 changeYear+"-"+(Integer.parseInt(changeMonth)+1) 는 10~12달을 표시한다.
 		 * 
-		 *  */
+		 */
 		nextMonth  = (Integer.parseInt(changeMonth)+1<10 ? changeYear+"-"+"0"+(Integer.parseInt(changeMonth)+1):
 			((Integer.parseInt(changeMonth)+1)>12 ? (Integer.parseInt(changeYear)+1)+"-"+"01":
 				changeYear+"-"+(Integer.parseInt(changeMonth)+1)))+"-"+"01"+" "+"00"+":"+"00"+":"+"00";
 		
 		
-		/** 계산한 값들을 jsp에 다시 적재한다 */
-		model.addAttribute("thisMonth",thisMonth);
-		model.addAttribute("nextMonth",nextMonth);
-		rttr.addFlashAttribute("message","success");
+		/** 계산한 값들을 jsp에 다시 적재 */
+		model.addAttribute("thisMonth", thisMonth);
+		model.addAttribute("nextMonth", nextMonth);
+		rttr.addFlashAttribute("message", "success");
 		
 		return "redirect:/council/councilSchedule";
+	}
+	
+	
+	
+	/** 모바일 전용 컨트롤러 */
+	
+	   
+	/**
+	 * 모바일용 학생회 일정 조회
+	 *
+	 * @Method Name : getCouncilScheduleMobile
+	 * @return
+	 */
+	@RequestMapping(value = "/scheduleMobile", method = RequestMethod.GET)
+	@ResponseBody
+	public Map<String, Object> getCouncilScheduleMobile () {
+		Map<String, Object> scheduleMap = new HashMap<String, Object>();
+
+		List<CouncilScheduleDTO> scheduleList =service.listAllSchedule();
+		
+		scheduleMap.put("scheduleList", scheduleList);
+		
+		return scheduleMap;
 	}
 }
--- Project_SKHUter/src/main/java/com/classs/skhuter/notice/controller/StuScheduleController.java
+++ Project_SKHUter/src/main/java/com/classs/skhuter/notice/controller/StuScheduleController.java
@@ -1,7 +1,9 @@
 package com.classs.skhuter.notice.controller;
 
 import java.util.Calendar;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import javax.inject.Inject;
 
@@ -12,11 +14,21 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.classs.skhuter.notice.domain.StuScheduleDTO;
 import com.classs.skhuter.notice.service.StuScheduleService;
 
+/**
+ * 학생회 일정 관련 프로젝트 Controller @RequestMapping("/notice/stuSchedule") URI 매칭
+ * 
+ * @패키지 : com.classs.skhuter.notice.controller
+ * @파일명 : StuScheduleController.java
+ * @작성자 : 나상연
+ * @작성일 : 2017. 10. 4. 
+ *
+ */
 @Controller
 @RequestMapping("/notice")
 public class StuScheduleController {
@@ -37,8 +49,11 @@ public class StuScheduleController {
 	public String currentM = Integer.toString(cal.get(Calendar.MONTH)+1);
 	
 	/**
-	 * 
-	 * 스케쥴 목록 불러오기
+	 * 학사 일정 전체 목록 조회
+	 *
+	 * @Method Name : TimeLine
+	 * @param model
+	 * @return
 	 */
 	@RequestMapping("/stuSchedule")
 	public String TimeLine(Model model) {
@@ -62,40 +77,57 @@ public class StuScheduleController {
 	}
 	
 	/**
-	 * 
-	 * 스케쥴 등록
+	 * 학사 일정 등록
+	 *
+	 * @Method Name : registSchedule
+	 * @param registeredSchedule
+	 * @param rttr
+	 * @return
 	 */
 	@RequestMapping(value="/stuSchedule/registSchedule", method = RequestMethod.POST)
 	public String registSchedule(StuScheduleDTO registeredSchedule,RedirectAttributes rttr) {
-		logger.info(registeredSchedule.toString());
 		service.register(registeredSchedule);
 		
 		rttr.addFlashAttribute("message", "createsuccess");
+		
 		return "redirect:/notice/stuSchedule";	
 	}
 	
 	/**
+	 * 학사 일정 삭제
 	 * 
-	 * 스케쥴 삭제
+	 *
+	 * @Method Name : deleteSchedule
+	 * @param schoolScheduleNo
+	 * @param rttr
+	 * @return
 	 */
 	@RequestMapping(value="/stuSchedule/deleteSchedule", method = RequestMethod.POST)
 	public String deleteSchedule(@RequestParam("schoolScheduleNo") int schoolScheduleNo,RedirectAttributes rttr) {
-		logger.info(schoolScheduleNo+"");
+		
 		service.deleteSchedule(schoolScheduleNo);
+		
 		rttr.addFlashAttribute("message", "deletesuccess");
+		
 		return "redirect:/notice/stuSchedule";
 	}
 	
+	/**
+	 * 날짜 변경
+	 *
+	 * @Method Name : changeDate
+	 * @param changeYear	변경한 해
+	 * @param changeMonth	변경한 달
+	 * @param rttr
+	 * @param model
+	 * @return
+	 */
 	@RequestMapping(value="/stuSchedule/changeDate",method = RequestMethod.POST)
 	public String changeDate(String changeYear,String changeMonth,RedirectAttributes rttr,Model model) {
 		currentY = changeYear;
 		currentM = changeMonth;
 		
-		/**
-		 * 일정이 끝나는 달을 고정시키기 위한 연산.
-		 * 
-		 * 
-		 */
+		/** 일정이 끝나는 달을 고정시키기 위한 연산 */
 		thisMonth = changeYear+"-"+(Integer.parseInt(changeMonth)<10?"0"+Integer.parseInt(changeMonth):
 			Integer.parseInt(changeMonth))+"-"+"01"+" "+"00"+":"+"00"+":"+"00";
 		
@@ -108,17 +140,40 @@ public class StuScheduleController {
 		 * Integer.parseInt(changeYear)+1)+"-"+"01" 로 년수를 1 증가시키고 달을 1월달로 만들어준다.
 		 * 세번째줄 changeYear+"-"+(Integer.parseInt(changeMonth)+1) 는 10~12달을 표시한다.
 		 * 
-		 *  */
+		 */
 		nextMonth  = (Integer.parseInt(changeMonth)+1<10 ? changeYear+"-"+"0"+(Integer.parseInt(changeMonth)+1):
 			((Integer.parseInt(changeMonth)+1)>12 ? (Integer.parseInt(changeYear)+1)+"-"+"01":
 				changeYear+"-"+(Integer.parseInt(changeMonth)+1)))+"-"+"01"+" "+"00"+":"+"00"+":"+"00";
 		
 		
-		/** 계산한 값들을 jsp에 다시 적재한다 */
+		/** 계산한 값들을 jsp에 다시 적재 */
 		model.addAttribute("thisMonth",thisMonth);
 		model.addAttribute("nextMonth",nextMonth);
 		rttr.addFlashAttribute("message","success");
 		
 		return "redirect:/notice/stuSchedule";
+	}
+	
+	
+	
+	/** 모바일 전용 컨트롤러 */
+	
+	   
+	/**
+	 * 모바일용 학사 일정 조회
+	 *
+	 * @Method Name : getStuScheduleMobile
+	 * @return
+	 */
+	@RequestMapping(value = "/stuScheduleMobile", method = RequestMethod.GET)
+	@ResponseBody
+	public Map<String, Object> getStuScheduleMobile () {
+		Map<String, Object> stuScheduleMap = new HashMap<String, Object>();
+		
+		List<StuScheduleDTO> scheduleList =service.listAllSchedule();
+		
+		stuScheduleMap.put("scheduleList", scheduleList);
+		
+		return stuScheduleMap;
 	}
 }
