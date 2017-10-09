fe6f7080e879fd0c29a0471f8b48c678d87835ad
xoxod <xoxod@192.168.10.4>
2017-10-09 19:30:14 +0900

    공지구현중

--- Project_SKHUter/src/main/java/com/classs/skhuter/notice/dao/NoticeDAOImpl.java
+++ Project_SKHUter/src/main/java/com/classs/skhuter/notice/dao/NoticeDAOImpl.java
@@ -1,5 +1,84 @@
 package com.classs.skhuter.notice.dao;
 
+import java.util.List;
+
+import javax.inject.Inject;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.stereotype.Repository;
+
+import com.classs.skhuter.notice.domain.NoticeDTO;
+import com.classs.skhuter.util.Criteria;
+@Repository
 public class NoticeDAOImpl implements NoticeDAO {
+	
+	@Inject
+	private SqlSession sqlSession;
+	
+	private static String namespace = "com.classs.skhuter.mappers.NoticeMapper";
+	@Override
+	public void create(NoticeDTO notice) {
+		sqlSession.insert(namespace + ".create", notice);
+	}
+
+	@Override
+	public NoticeDTO read(int noticeNo) throws Exception {
+		// TODO Auto-generated method stub
+		return sqlSession.selectOne(namespace + ".read", noticeNo);
+	}
+
+	@Override
+	public void delete(int noticeNo) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public List<NoticeDTO> listCriteria(Criteria cri) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int countPaging(Criteria cri) throws Exception {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public List<NoticeDTO> listSearch_t(Criteria cri) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int listSearchCount_t(Criteria cri) throws Exception {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public List<NoticeDTO> listSearch_c(Criteria cri) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int listSearchCount_c(Criteria cri) throws Exception {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public List<NoticeDTO> listSearch_tc(Criteria cri) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int listSearchCount_tc(Criteria cri) throws Exception {
+		// TODO Auto-generated method stub
+		return 0;
+	}
 
 }
--- Project_SKHUter/src/main/java/com/classs/skhuter/notice/domain/NoticeDTO.java
+++ Project_SKHUter/src/main/java/com/classs/skhuter/notice/domain/NoticeDTO.java
@@ -1,5 +1,73 @@
 package com.classs.skhuter.notice.domain;
 
-public class NoticeDTO {
+import java.sql.Date;
 
+public class NoticeDTO {
+	
+	private int noticeNo;
+	private int userNo;
+	private String title;
+	private Date regdate;
+	private int hitCount;
+	private String content;
+	private Date modifyDate;
+	private int exposure;
+	
+	
+	public int getNoticeNo() {
+		return noticeNo;
+	}
+	public void setNoticeNo(int noticeNo) {
+		this.noticeNo = noticeNo;
+	}
+	public int getUserNo() {
+		return userNo;
+	}
+	public void setUserNo(int userNo) {
+		this.userNo = userNo;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public Date getRegdate() {
+		return regdate;
+	}
+	public void setRegdate(Date regdate) {
+		this.regdate = regdate;
+	}
+	public int getHitCount() {
+		return hitCount;
+	}
+	public void setHitCount(int hitCount) {
+		this.hitCount = hitCount;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public Date getModifyDate() {
+		return modifyDate;
+	}
+	public void setModifyDate(Date modifyDate) {
+		this.modifyDate = modifyDate;
+	}
+	public int getExposure() {
+		return exposure;
+	}
+	public void setExposure(int exposure) {
+		this.exposure = exposure;
+	}
+	
+	@Override
+	public String toString() {
+		return "NoticeDTO [noticeNo=" + noticeNo + ", userNo=" + userNo + ", title=" + title + ", regdate=" + regdate
+				+ ", hitCount=" + hitCount + ", content=" + content + ", modifyDate=" + modifyDate + ", exposure="
+				+ exposure + "]";
+	}
+	
 }
